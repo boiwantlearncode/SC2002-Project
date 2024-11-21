8eb88b0ab97a5f76772b13efa06e6f84cf3d1d98
Ian Koh Jin <149784365+IKJ9@users.noreply.github.com>
2024-11-21 14:06:39 +0800

    Update ReplenishmentRequestRepo.java

--- src/DataManager/ReplenishmentRequestRepo.java
+++ src/DataManager/ReplenishmentRequestRepo.java
@@ -8,9 +8,31 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The ReplenishmentRequestRepo class manages the persistence of a list of replenishment requests.
+ * It provides functionality to save, load, and retrieve the list of replenishment requests using serialization.
+ */
+
 public class ReplenishmentRequestRepo implements SerializableRepo<List<ReplenishmentRequest>> {
+
+    /**
+     * Path to the file where the replenishment request data is stored.
+     */
+    
     private static final String REPLENISHMENT_REQUESTS_FILE = "src/DataManager/replenishment_requests.txt";
+
+    /**
+     * The list of replenishment requests managed by this repository.
+     */
+    
     private List<ReplenishmentRequest> replenishmentRequests = new ArrayList<>();
+
+    /**
+     * Saves the current list of replenishment requests to a file using serialization.
+     * If the file does not exist, it will be created.
+     *
+     * @throws IOException if an I/O error occurs while writing to the file.
+     */
 
     @Override
     public void saveData() throws IOException {
@@ -19,6 +41,14 @@ public class ReplenishmentRequestRepo implements SerializableRepo<List<Replenish
             //System.out.println("Replenishment requests data saved successfully.");
         }
     }
+
+    /**
+     * Loads the list of replenishment requests from a file using deserialization.
+     * If the file does not exist or is empty, it initializes the list as empty.
+     *
+     * @throws IOException if an I/O error occurs while reading the file.
+     * @throws ClassNotFoundException if the class of the serialized object cannot be found.
+     */
 
     @Override
     public void loadData() throws IOException, ClassNotFoundException {
@@ -35,10 +65,22 @@ public class ReplenishmentRequestRepo implements SerializableRepo<List<Replenish
         }
     }
 
+    /**
+     * Retrieves the current list of replenishment requests.
+     *
+     * @return the list of replenishment requests.
+     */
+
     @Override
     public List<ReplenishmentRequest> getData() {
         return replenishmentRequests;
     }
+
+    /**
+     * Sets the list of replenishment requests to the specified value.
+     *
+     * @param replenishmentRequests the list of replenishment requests to be set.
+     */
 
     public void setReplenishmentRequests(List<ReplenishmentRequest> replenishmentRequests) {
         this.replenishmentRequests = replenishmentRequests;
