d0499b371f14681843624416367142395bb2c8b6
Ian Koh Jin <149784365+IKJ9@users.noreply.github.com>
2024-11-21 14:04:34 +0800

    Update InventoryRepo.java

--- src/DataManager/InventoryRepo.java
+++ src/DataManager/InventoryRepo.java
@@ -9,9 +9,32 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The InventoryRepo class manages the persistence of the Inventory object. 
+ * It provides functionality to save, load, and retrieve inventory data using serialization.
+ * If the inventory file is empty, it initializes the inventory with sample data.
+ */
+
 public class InventoryRepo implements SerializableRepo<Inventory> {
+
+    /**
+     * Path to the file where the inventory data is stored.
+     */
+    
     private static final String INVENTORY_FILE = "src/DataManager/inventory.txt";
+
+    /**
+     * The Inventory object managed by this repository.
+     */
+    
     private Inventory inventory = new Inventory();
+
+    /**
+     * Saves the current inventory data to a file using serialization.
+     * If the file does not exist, it will be created.
+     *
+     * @throws IOException if an I/O error occurs while writing to the file.
+     */
 
     @Override
     public void saveData() throws IOException {
@@ -20,6 +43,14 @@ public class InventoryRepo implements SerializableRepo<Inventory> {
             //System.out.println("Inventory data saved successfully.");
         }
     }
+
+    /**
+     * Loads the inventory data from a file using deserialization.
+     * If the file does not exist or is empty, it initializes the inventory with sample data.
+     *
+     * @throws IOException if an I/O error occurs while reading the file.
+     * @throws ClassNotFoundException if the class of the serialized object cannot be found.
+     */
 
     @Override
     public void loadData() throws IOException, ClassNotFoundException {
@@ -43,10 +74,22 @@ public class InventoryRepo implements SerializableRepo<Inventory> {
         }
     }
 
+    /**
+     * Retrieves the current inventory data.
+     *
+     * @return the Inventory object containing the current inventory data.
+     */
+
     @Override
     public Inventory getData() {
         return inventory;
     }
+
+    /**
+     * Sets the inventory object to the specified value.
+     *
+     * @param inventory the Inventory object to be set.
+     */
 
     public void setInventory(Inventory inventory) {
         this.inventory = inventory;
